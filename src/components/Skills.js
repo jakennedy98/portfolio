8a945b8bcb08a12a1de7955d739af24bbb38eaa8
Jacob Kennedy <jakennedy98@gmail.com>
2022-02-17 14:49:15 +0100

    star background

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,52 +8,52 @@ function Skills() {
             <ul className="skills-grid">
                 <li className="skills-wrapper">
                     <div className="skill-title">HTML5</div>             
-                    <img src = "/html5-icon.png" alt="HTML5" className="skill-logo" height="60px" />
+                    <img src = "/html5-icon.png" alt="HTML5" className="skill-logo" height="60px" loading = "lazy" />
                 </li>
    
                 <li className="skills-wrapper">
                     <div>CSS</div>
-                    <img src = "/css-icon.png" alt="CSS" className="skill-logo" width="60px"  />
+                    <img src = "/css-icon.png" alt="CSS" className="skill-logo" width="60px"  loading = "lazy" />
                 </li>
 
                 <li className="skills-wrapper">
                     <div>Javascript</div>
-                    <img src = "/javascript-icon.png" alt="Javascript" className="skill-logo" width="60px"  />
+                    <img src = "/javascript-icon.png" alt="Javascript" className="skill-logo" width="60px" loading = "lazy"   />
                 </li>
 
                 <li className="skills-wrapper">
                     <div>React</div>
-                    <img src = "/react-icon.png" alt="Javascript" className="skill-logo" width="60px"  />
+                    <img src = "/react-icon.png" alt="Javascript" className="skill-logo" width="60px"  loading = "lazy" />
                 </li>
 
                 <li className="skills-wrapper">
                     <div>Git / Github</div>
-                    <img src = "/github.png" alt="Javascript" className="skill-logo" width="56px"  />
+                    <img src = "/github.png" alt="Javascript" className="skill-logo" width="56px"  loading = "lazy" />
                 </li>
                 
                 <li className="skills-wrapper">
                     <div>RESTful APIs</div>
-                    <img src = "/api.png" alt="Javascript" className="skill-logo" width="48px"  />
+                    <img src = "/api.png" alt="Javascript" className="skill-logo" width="48px" loading = "lazy"  />
                 </li>
 
                 <li className="skills-wrapper">
                     <div>Python</div>
-                    <img src = "/python-icon.png" alt="Javascript" className="skill-logo" width="48px"  />
+                    <img src = "/python-icon.png" alt="Javascript" className="skill-logo" width="48px" loading = "lazy"  />
                 </li>
 
                 <li className="skills-wrapper">
                     <div>Django</div>
-                    <img src = "/django-icon.png" alt="Javascript" className="skill-logo" width="48px"  />
+                    <img src = "/django-icon.png" alt="Javascript" className="skill-logo" width="48px"  loading = "lazy" />
                 </li>
 
                 <li className="skills-wrapper">
                     <div>SQL</div>
-                    <img src = "/sql-server.png" alt="Javascript" className="skill-logo" width="50px"  />
+                    <img src = "/sql-server.png" alt="Javascript" className="skill-logo" width="50px" loading = "lazy"  />
                 </li>
 
                 <li className="skills-wrapper">
                     <div>R</div>
-                    <img src = "/R-icon.png" alt="Javascript" className="skill-logo" width="48px"  />
+                    <img src = "/R-icon.png" alt="Javascript" className="skill-logo" width="48px"  loading = "lazy" />
                 </li>
 
                
